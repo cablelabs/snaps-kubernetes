c18ae13c52b7e328863edccb180cd04b3b4d07a8
Chirag Gupta <chirag.gupta@aricent.com>
2018-06-12 17:47:50 +0530

    change copyright

--- snaps_k8s/ansible_p/commission/kubernetes/playbooks/deploy_mode/k8/__init__.py
+++ snaps_k8s/ansible_p/commission/kubernetes/playbooks/deploy_mode/k8/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2017 ARICENT HOLDINGS LUXEMBOURG SARL and Cable Television
+# Copyright 2018 ARICENT HOLDINGS LUXEMBOURG SARL and Cable Television
 # Laboratories, Inc.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
