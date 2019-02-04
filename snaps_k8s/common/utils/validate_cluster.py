4ce746be24a9c9776c02e13e850764526ab1fee2
spisarski <s.pisarski@cablelabs.com>
2019-02-04 16:32:33 -0700

    Replaced assert statements with if/then raise ClusterDeploymentException

--- snaps_k8s/common/utils/validate_cluster.py
+++ snaps_k8s/common/utils/validate_cluster.py
@@ -118,33 +118,57 @@ def validate_nodes(k8s_conf):
         kubelet_reason = False
         for node_condition in node_conditions:
             if node_condition.reason == 'KubeletReady':
-                assert node_condition.status == 'True'
-                assert node_condition.type == 'Ready'
-                kubelet_reason = True
-        assert kubelet_reason
+                if node_condition.status != 'True':
+                    raise ClusterDeploymentException(
+                        'node_condition.status is [{}]'.format
+                        (node_condition.status))
+                if node_condition.type != 'Ready':
+                    raise ClusterDeploymentException(
+                        'node_condition.type is [{}]'.format(
+                            node_condition.type))
+            kubelet_reason = True
+        if not kubelet_reason:
+            raise ClusterDeploymentException(
+                'Could not determine the state of all nodes')
 
         node_info = node_status.node_info
         node_kubelet_version = node_info.kubelet_version
         expected_version = config_utils.get_version(k8s_conf)
-        assert node_kubelet_version == expected_version
+        if node_kubelet_version != expected_version:
+            raise ClusterDeploymentException(
+                'Unexpected kubelet_version [{}]'.format(node_kubelet_version))
+
         logger.debug('Expected version [%s] == actual [%s]',
                      expected_version, node_kubelet_version)
 
         node_name = node_meta.name
         node_labels = node_meta.labels
         if node_labels.get('node-role.kubernetes.io/master') is not None:
-            assert node_name in master_names
+            if node_name not in master_names:
+                raise ClusterDeploymentException(
+                    'Node [{}] is not a master'.format(node_name))
+
             master_count += 1
             logger.debug('Master found with name [%s]', node_name)
 
         if node_labels.get('node-role.kubernetes.io/node') is not None:
-            assert node_name in minion_names
+            if node_name not in minion_names:
+                raise ClusterDeploymentException(
+                    'Node [{}] is not a minion'.format(node_name))
+
             minion_count += 1
             logger.debug('Minion found with name [%s]', node_name)
 
-    assert master_count == len(masters_tuple3)
+    if master_count != len(masters_tuple3):
+        raise ClusterDeploymentException(
+            'Expected number of masters [{}] - actual [%s]'.format(
+                len(masters_tuple3), master_count))
     logger.info('Number of masters [%s]', master_count)
-    assert minion_count == len(minions_tuple3)
+
+    if minion_count != len(minions_tuple3):
+        raise ClusterDeploymentException(
+            'Expected number of minions [{}] - actual [{}]'.format(
+                len(minions_tuple3), minion_count))
     logger.info('Number of minions [%s]', minion_count)
 
 
@@ -162,30 +186,40 @@ def validate_k8s_system(k8s_conf):
     pod_status = __get_pod_name_statuses(pod_items)
     for pod_name, pod_running in pod_status.items():
         if not pod_running:
-            logger.warn('Pod %s is not running', pod_name)
-        assert pod_running
+            raise ClusterDeploymentException(
+                'Pod [{}] is not running as expected'.format(pod_name))
 
     pod_services = __get_pod_service_list(pod_items)
     logger.debug('pod_services - %s', pod_services)
-    assert 'kubernetes-dashboard' in pod_services
-    assert 'kube-dns' in pod_services
-    assert 'efk' in pod_services
+    if 'kubernetes-dashboard' not in pod_services:
+        raise ClusterDeploymentException(
+            'kubernetes-dashboard service not found')
+
+    if 'kube-dns' not in pod_services:
+        raise ClusterDeploymentException('kube-dns service not found')
+
+    if 'efk' not in pod_services:
+        raise ClusterDeploymentException('efk service not found')
 
     for name, ip, node_type in config_utils.get_master_nodes_ip_name_type(
             k8s_conf):
-        assert 'kube-apiserver-{}'.format(name) in pod_services
-        assert 'kube-scheduler-{}'.format(name) in pod_services
+        if 'kube-apiserver-{}'.format(name) not in pod_services:
+            raise ClusterDeploymentException(
+                'kube-apiserver-%s service not found', name)
+        if 'kube-scheduler-{}'.format(name) not in pod_services:
+            raise ClusterDeploymentException(
+                'kube-scheduler-%s service not found', name)
 
     if config_utils.is_metrics_server_enabled(k8s_conf):
-        assert 'metrics-server' in pod_services
-    else:
-        assert 'metrics-server' not in pod_services
+        if 'metrics-server' not in pod_services:
+            raise ClusterDeploymentException(
+                'metrics-server service not found')
 
     logger.info('pod_services - %s', pod_services)
     if config_utils.is_helm_enabled(k8s_conf):
-        assert 'tiller' in pod_services
-    else:
-        assert 'tiller' not in pod_services
+        if 'tiller' not in pod_services:
+            raise ClusterDeploymentException(
+                'tiller service not found')
 
 
 def validate_cni(k8s_conf):
@@ -213,15 +247,21 @@ def __validate_cni_pods(k8s_conf):
     logger.debug('pod_services - %s', pod_services)
     net_plugin = config_utils.get_networking_plugin(k8s_conf)
     if net_plugin == consts.WEAVE_TYPE:
-        assert 'weave-net' in pod_services
+        if 'weave-net' not in pod_services:
+            raise ClusterDeploymentException('weave-net service not found')
     elif net_plugin == consts.FLANNEL_TYPE:
-        assert 'flannel' in pod_services
+        if 'flannel' not in pod_services:
+            raise ClusterDeploymentException('flannel service not found')
     elif net_plugin == 'contiv':
-        assert 'contiv-netplugin' in pod_services
+        if 'contiv-netplugin' not in pod_services:
+            raise ClusterDeploymentException(
+                'contiv-netplugin service not found')
     elif net_plugin == 'calico':
-        assert 'calico-net' in pod_services
+        if 'calico-net' not in pod_services:
+            raise ClusterDeploymentException('calico-net service not found')
     elif net_plugin == 'cilium':
-        assert 'cilium-net' in pod_services
+        if 'cilium-net' not in pod_services:
+            raise ClusterDeploymentException('cilium-net service not found')
 
 
 def __validate_cni_networks(k8s_conf):
@@ -267,16 +307,27 @@ def __validate_host_vols(k8s_conf):
     host_vol_conf = __get_host_vol_dict(k8s_conf)
     for pv in pv_list.items:
         pv_name = pv.metadata.name
-        assert host_vol_conf.get(pv_name) is not None
+        if not host_vol_conf.get(pv_name):
+            raise ClusterDeploymentException(
+                'Config for host volume [{}] not found'.format(pv_name))
+
         pv_size = pv.spec.capacity['storage']
-        assert pv_size == host_vol_conf.get(pv_name)
+        if pv_size != host_vol_conf.get(pv_name):
+            raise ClusterDeploymentException(
+                'Expected vol size [{}] not actual [{}]'.format(
+                    host_vol_conf.get(pv_name), pv_size))
 
     pv_claims = core_client.list_persistent_volume_claim_for_all_namespaces()
     for pv_claim in pv_claims.items:
         pvc_name = pv_claim.metadata.name
-        assert host_vol_conf.get(pvc_name) is not None
+        if not host_vol_conf.get(pvc_name):
+            raise ClusterDeploymentException(
+                'PVC [{}] not configured'.format(pvc_name))
         pvc_size = pv_claim.status.capacity['storage']
-        assert pvc_size == host_vol_conf.get(pvc_name)
+        if pvc_size != host_vol_conf.get(pvc_name):
+            raise ClusterDeploymentException(
+                'PVC expected size [{}] - actual [{}]'.format(
+                    host_vol_conf.get(pvc_name), pvc_size))
 
 
 def __get_host_vol_dict(k8s_conf):
@@ -345,3 +396,9 @@ def __get_pod_service_list(pod_items):
         else:
             out_names.add(pod_item.metadata.name)
     return out_names
+
+
+class ClusterDeploymentException(Exception):
+    """
+    Exception to raise when there are issues with the K8s deployment
+    """
