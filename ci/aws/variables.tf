e820dd2ca6a1bff0815e472b403399e0e649fcca
Bo Quan <b.quan@cablelabs.com>
2019-07-26 13:51:02 -0600

    Add stub variables to remove TF warnings.

--- ci/aws/variables.tf
+++ ci/aws/variables.tf
@@ -16,6 +16,10 @@ variable "access_key" {}
 variable "secret_key" {}
 variable "build_id" {}
 
+# Stub Variables (unused but to remove warnings)
+variable "git_user" {}
+variable "git_pass" {}
+
 # Optional Variables
 variable "public_key_file" {default = "~/.ssh/id_rsa.pub"}
 variable "private_key_file" {default = "~/.ssh/id_rsa"}
